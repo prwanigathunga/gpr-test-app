bbad831dc1b19c45b5767c6caee21da3ee51a5bb
ranga <prwanigathunga@gmail.com>
2024-02-25 12:24:30 +0530

    initial commit

---
+++ src/main/java/com/gpr/gprtestapp/GPRController.java
@@ -0,0 +1,8 @@
+package com.gpr.gprtestapp;
+
+public class GPRController {
+
+    public void gprFunction(){
+        System.out.println("this is a test ..................................................");
+    }
+}
---
+++ src/test/java/com/gpr/gprtestapp/GprTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpr.gprtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GprTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
